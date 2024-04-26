a4f831474cd37ef4340489bcdacbe00a1ad09ef4
deeku02 <jaindeeksha0204@gmail.com>
2024-04-26 13:44:21 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,14 +1,3 @@
-def smallest_missing_positive_integer(numbers):
-    smallest_missing = 1
-    for num in numbers:
-        if num > 0 and num <= smallest_missing:
-            smallest_missing = num + 1
-    return smallest_missing
-
-
-
-    
-
 
 
   
