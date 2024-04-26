23fabdee83eb4a63a36e1fbebde9c5eebeee43b0
Deeksha Jain <95418183+deeku02@users.noreply.github.com>
2024-04-26 13:02:22 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,13 +1,26 @@
-def longest_substring(s: str) -> int:
-    
-    """"
-    
-    Implement the function longest_substring 
-    using the provided longest_substring function to find 
-    the length of the longest substring without repeating characters.
+def longest_substring(s):
+    """
+    Implementation of the longestSubstring function.
+    """
+    longest_substring_length = 0
 
-    """ 
-    pass
+    for i in range(len(s)):
+        current_string_set = set()
+
+        for x in range(i, len(s)):
+            if s[x] in current_string_set:
+                break
+            else:
+                current_string_set.add(s[x])
+
+        longest_substring_length = max(
+            longest_substring_length,
+            len(current_string_set)
+        )
+
+    return longest_substring_length
+
+
 
 
 
