356684378f4ad6e8f37db0d44bca570ad762d969
Deeksha Jain <95418183+deeku02@users.noreply.github.com>
2024-04-26 13:05:03 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,18 +1,9 @@
-def smallest_missing_positive_integer(nums):
-    nums = [num for num in nums if num > 0]
-
-    for i in range(len(nums)):
-        index = abs(nums[i]) - 1
-        if 0 <= index < len(nums) and nums[index] > 0:
-            nums[index] = -nums[index]
-
-    for i in range(len(nums)):
-        if nums[i] > 0:
-            return i + 1
-
-    return len(nums) + 1
-
-
+def smallest_missing_positive_integer(numbers):
+    smallest_missing = 1
+    for num in numbers:
+        if num > 0 and num <= smallest_missing:
+            smallest_missing = num + 1
+    return smallest_missing
 
 
 
