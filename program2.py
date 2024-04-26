8d39116d65d1b7725cea8231151f31657734ea54
deeku02 <jaindeeksha0204@gmail.com>
2024-04-26 13:44:02 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -21,21 +21,3 @@ def longest_substring(s):
     return longest_substring_length
 
 
-
-    for i in range(len(s)):
-        current_string_set = set()
-
-        for x in range(i, len(s)):
-            if s[x] in current_string_set:
-                break
-            else:
-                current_string_set.add(s[x])
-
-        longest_substring_length = max(
-            longest_substring_length,
-            len(current_string_set)
-        )
-
-    return longest_substring_length
-
-
